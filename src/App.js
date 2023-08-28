7bb1bf5767cb41dcf9e5c8ca62746193779ee8ab
sivasaireddys7 <Sivasaip@s7works.io>
2023-08-28 10:32:53 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <HeadlineCards/>
+      <Food/>
+    </div>
+  );
+}
+
+export default App;
